029c1818095dbaf7a2c1b2b7744bfd5f5be8b708
sunerok <justinvforvendetta@users.noreply.github.com>
2015-11-07 00:21:06 -0500

    Update transaction.py

--- lib/transaction.py
+++ lib/transaction.py
@@ -588,7 +588,7 @@ class Transaction:
                 script = '76a9'                                      # op_dup, op_hash_160
                 script += push_script(hash_160.encode('hex'))
                 script += '88ac'                                     # op_equalverify, op_checksig
-            elif addrtype == 16:
+            elif addrtype == 28:
                 script = 'a9'                                        # op_hash_160
                 script += push_script(hash_160.encode('hex'))
                 script += '87'                                       # op_equal
